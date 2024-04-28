fcbb48fd7ed2dbfad0b52b3d4ad6d9325106e46a
titoiriepal <titoiriepal@hotmail.com>
2024-04-28 19:05:24 +0200

    Query change from AdminController index

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -22,14 +22,14 @@ class AdminController{
 
         //Consultar la base de datos
         $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
-        $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, citasServicios.precio, citas.activo  ";
+        $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, citasservicios.precio, citas.activo  ";
         $consulta .= " FROM citas  ";
         $consulta .= " LEFT OUTER JOIN usuarios ";
         $consulta .= " ON citas.usuarioId=usuarios.id  ";
-        $consulta .= " LEFT OUTER JOIN citasServicios ";
-        $consulta .= " ON citasServicios.citaId=citas.id ";
+        $consulta .= " LEFT OUTER JOIN citasservicios ";
+        $consulta .= " ON citasservicios.citaId=citas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
-        $consulta .= " ON servicios.id=citasServicios.servicioId ";
+        $consulta .= " ON servicios.id=citasservicios.servicioId ";
         $consulta .= " WHERE fecha =  '$fecha' ";
         if ($tipoCita === "canceladas"){
             $consulta .= "AND citas.activo = 0 ";
